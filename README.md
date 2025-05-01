2950f9b2f4e02679e69789ebb333a947b2aa18ba
Asami De Almeida <stem@wraith.red>
2025-05-01 18:06:39 +1200

    Refactoring Code & Docs, Removed Old Code

--- README.md
+++ README.md
@@ -1,23 +1,34 @@
 # StenoByte Prototype
-A stenotype keyboard for typing out bytes. Uses certain keys from your existing ASCII keyboard layout.
+A stenotype keyboard for typing out bytes. Uses certain keys from your existing QWERTY keyboard layout.
+This is currently designed to work in Linux.
 
-# Key Codes
-* a - 97
-* s - 115
-* d - 100
-* f - 102
+# Codes
+When developing this application, the following codes from the keyboard correspond to the following keys and events.
 
-* j - 106
-* k - 107
-* l - 108
-* ; - 59
+## Keys
+* a - 30
+* s - 31
+* d - 32
+* f - 33
 
-* SPACE - 32
+* j - 36
+* k - 37
+* l - 38
+* ; - 39
 
-## Known Dependencies
-* ncurses.h (old)
+* SPACE - 57
+
+## Events
+* 0 - Released
+* 1 - Pressed
+* 2 - Repeated
+
+
+
+# Known Dependencies
 * libevdev
 
 # Resources Referenced
-* https://stackoverflow.com/questions/4025891/create-a-function-to-check-for-key-press-in-unix-using-ncurses
-* https://stackoverflow.com/questions/26693381/cmake-does-not-link-ncurseszxxxxs
+* https://www.freedesktop.org/software/libevdev/doc/latest/index.html
+* ChatGPT to generate example code to start from (prompt: "I would like to detect which keys are still pressed and
+which are not")
--- main.c
+++ main.c
@@ -6,50 +6,52 @@
 
 int main() {
     // Struct to store the evdev device
-    struct libevdev *dev = nullptr;
+    struct libevdev *keyboard_device = nullptr;
 
     // Opens the keyboard event file (usually event3) in Read Only and Non-Blocking Modes
     // Reports an error if something went wrong
-    const int fd = open("/dev/input/event3", O_RDONLY | O_NONBLOCK); // Change to correct device
-    if (fd < 0) {
+    const int event_file_device = open("/dev/input/event3", O_RDONLY | O_NONBLOCK); // Change to correct device
+    if (event_file_device < 0) {
         perror("Failed to open device");
         return 1;
     }
 
-    // Initialises the evdev device (stored in libevdev struct named dev)
+    // Initialises the evdev device (stored in libevdev struct named "keyboard_device"))
     // Reports an error if evdev initialisation failed
-    if (libevdev_new_from_fd(fd, &dev) < 0) {
+    if (libevdev_new_from_fd(event_file_device, &keyboard_device) < 0) {
         perror("Failed to init libevdev");
         return 1;
     }
 
     // Prints evdev device name
-    printf("Input device name: %s\n", libevdev_get_name(dev));
+    printf("Input device name: %s\n", libevdev_get_name(keyboard_device));
     printf("Press ESC to exit\n");
 
 
-    struct input_event ev;  // The current event
+    struct input_event current_event;  // The current event struct
     while (1) {
-        const int rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL, &ev);  // Gets the next event
+        // Gets the next event
+        const int next_event_result_code = libevdev_next_event(keyboard_device,
+            LIBEVDEV_READ_FLAG_NORMAL, &current_event);
 
         // Ignores Non-Success Read Events
-        if (rc != LIBEVDEV_READ_STATUS_SUCCESS) {
+        if (next_event_result_code != LIBEVDEV_READ_STATUS_SUCCESS) {
             continue;
         }
 
         // Ensures a Key Event Type Occurred, ignores otherwise
-        if (ev.type != EV_KEY) {
+        if (current_event.type != EV_KEY) {
             continue;
         }
 
         // Prints the Key Event Summary
         printf("\nKey %s (%d): code %d\n\n",
-               ev.value ? (ev.value == 1 ? "PRESSED" : "REPEATED") : "RELEASED",
-               ev.value,
-               ev.code);
+               current_event.value ? (current_event.value == 1 ? "PRESSED" : "REPEATED") : "RELEASED",
+               current_event.value,
+               current_event.code);
 
         // If ESC Key is pushed, then exit app
-        if (ev.code == KEY_ESC) {
+        if (current_event.code == KEY_ESC) {
             printf("ESC pressed\nExiting...\n");
             break;
         }
@@ -58,7 +60,7 @@ int main() {
     }
 
     // Frees up resources before application ends
-    libevdev_free(dev);
-    close(fd);
+    libevdev_free(keyboard_device);
+    close(event_file_device);
     return 0;
 }
