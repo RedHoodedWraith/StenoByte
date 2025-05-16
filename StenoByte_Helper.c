814b1ee82630f3ab58437924ae17e423cc202622
Asami De Almeida <stem@redwraith.me>
2025-05-16 15:38:45 +1200

    Moved Code into Dedicated Methods

--- StenoByte_Helper.c
+++ StenoByte_Helper.c
@@ -219,3 +219,85 @@ void disable_echo() {
 void restore_terminal() {
     tcsetattr(STDIN_FILENO, TCSANOW, &original_terminal_settings); // restore settings
 }
+
+/*
+ * Sets up the application and configures the devices to read from
+ */
+void setup_stenobyte() {
+    printf("Starting StenoType...\n");
+
+    setup_subvalues_array();
+
+    // Struct to store the evdev device
+    struct libevdev *keyboard_device = nullptr;
+
+    // Opens the keyboard event file (usually event3) in Read Only and Non-Blocking Modes
+    // Reports an error if something went wrong
+    const int event_file_device = open("/dev/input/event3", O_RDONLY | O_NONBLOCK); // Change to correct device
+    if (event_file_device < 0) {
+        perror("Failed to open device");
+        return 1;
+    }
+
+    // Disables printing inputs to the terminal
+    disable_echo();
+
+    // Initialises the evdev device (stored in libevdev struct named "keyboard_device")
+    // Reports an error if evdev initialisation failed
+    if (libevdev_new_from_fd(event_file_device, &keyboard_device) < 0) {
+        perror("Failed to init libevdev");
+        return 1;
+    }
+
+    // Prints evdev device name
+    printf("Input device name: %s\n", libevdev_get_name(keyboard_device));
+    printf("Press ESC to exit\n");
+}
+
+/*
+ * Runs the loop that constantly checks the keyboard events and performs the associated actions
+ */
+void run_stenobyte() {
+    struct input_event current_event;  // The current event struct
+
+    print_bit_arr_summary();    // Initial Print Summary
+
+    // Infinitely loops by default
+    while (1) {
+        // Gets the next event
+        const int next_event_result_code = libevdev_next_event(keyboard_device,
+            LIBEVDEV_READ_FLAG_NORMAL, &current_event);
+
+        // Ignores Non-Success Read Events
+        if (next_event_result_code != LIBEVDEV_READ_STATUS_SUCCESS) {
+            continue;
+        }
+
+        // Ensures a Key Event Type Occurred, ignores otherwise
+        if (current_event.type != EV_KEY) {
+            continue;
+        }
+
+        // If ESC Key is pushed, then exit app
+        if (current_event.code == KEY_ESC) {
+            printf("ESC pressed\nExiting...\n");
+            break;
+        }
+
+        process_key_presses(&current_event);
+        print_bit_arr_summary();
+
+        if (ready_to_compute_byte) {
+            compute_byte();
+        }
+
+        usleep(1000); // Small delay
+    }
+}
+
+void end_stenobyte() {
+    // Frees up resources before application ends
+    libevdev_free(keyboard_device);
+    restore_terminal(); // Restores printing inputs to the terminal
+    close(event_file_device);
+}
--- main.c
+++ main.c
@@ -20,74 +20,10 @@
 
 
 int main() {
-    printf("Starting StenoType...\n");
-    // Struct to store the evdev device
-    struct libevdev *keyboard_device = nullptr;
+    setup_stenobyte();
 
-    setup_subvalues_array();
+    run_stenobyte();
 
-    // Opens the keyboard event file (usually event3) in Read Only and Non-Blocking Modes
-    // Reports an error if something went wrong
-    const int event_file_device = open("/dev/input/event3", O_RDONLY | O_NONBLOCK); // Change to correct device
-    if (event_file_device < 0) {
-        perror("Failed to open device");
-        return 1;
-    }
-
-    // Disables printing inputs to the terminal
-    disable_echo();
-
-    // Initialises the evdev device (stored in libevdev struct named "keyboard_device")
-    // Reports an error if evdev initialisation failed
-    if (libevdev_new_from_fd(event_file_device, &keyboard_device) < 0) {
-        perror("Failed to init libevdev");
-        return 1;
-    }
-
-    // Prints evdev device name
-    printf("Input device name: %s\n", libevdev_get_name(keyboard_device));
-    printf("Press ESC to exit\n");
-
-
-    struct input_event current_event;  // The current event struct
-
-    print_bit_arr_summary();    // Initial Print Summary
-
-    // Infinitely loops by default
-    while (1) {
-        // Gets the next event
-        const int next_event_result_code = libevdev_next_event(keyboard_device,
-            LIBEVDEV_READ_FLAG_NORMAL, &current_event);
-
-        // Ignores Non-Success Read Events
-        if (next_event_result_code != LIBEVDEV_READ_STATUS_SUCCESS) {
-            continue;
-        }
-
-        // Ensures a Key Event Type Occurred, ignores otherwise
-        if (current_event.type != EV_KEY) {
-            continue;
-        }
-
-        // If ESC Key is pushed, then exit app
-        if (current_event.code == KEY_ESC) {
-            printf("ESC pressed\nExiting...\n");
-            break;
-        }
-
-        process_key_presses(&current_event);
-        print_bit_arr_summary();
-
-        if (ready_to_compute_byte) {
-            compute_byte();
-        }
-
-        usleep(1000); // Small delay
-    }
-
-    // Frees up resources before application ends
-    libevdev_free(keyboard_device);
-    restore_terminal(); // Restores printing inputs to the terminal
-    close(event_file_device);
+    end_stenobyte();
     return 0;
 }
